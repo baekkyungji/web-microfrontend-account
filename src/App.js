2f8d05fc8f88bccb49a88c604dcf9a8c948a8cc0
Surahutomo Pradana <surahutomo.pradana@btpn.com>
2021-09-23 10:12:03 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {Component} from "react";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import React, { Component} from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Account from "./Account";
 
-class App extends Component {
+class App extends PureComponent {
   render() {
     return (
       <BrowserRouter>
