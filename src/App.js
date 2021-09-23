8b370875f51c1aadf9c9521f2ecf0f7df8017e71
R Surahutomo Aziz Pradana <pradana@Rs-MacBook-Pro.local>
2021-09-23 15:01:16 +0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component} from "react";
+import React, { PureComponent} from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Account from "./Account";
